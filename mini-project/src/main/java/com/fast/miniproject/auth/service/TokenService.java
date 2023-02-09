d021c33162b34d63ea7700b18f36aad6f9c229aa
cutegyuseok <yigyuseok@gmail.com>
2023-02-10 03:04:38 +0900

    feat: Service interface

--- mini-project/src/main/java/com/fast/miniproject/auth/service/TokenService.java
+++ mini-project/src/main/java/com/fast/miniproject/auth/service/TokenService.java
@@ -1,31 +1,7 @@
 package com.fast.miniproject.auth.service;
 
-import com.fast.miniproject.auth.entity.Token;
-import com.fast.miniproject.auth.repository.TokenRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
+public interface TokenService {
 
-@Service
-@RequiredArgsConstructor
-public class TokenService {
-
-    private final TokenRepository tokenRepository;
-
-    public String logout(String header){
-        if (checkLogout(header)){
-            return "already";
-        }else {
-            try {
-                tokenRepository.save(Token.builder().token(header).build());
-                return "success";
-            } catch (Exception e) {
-                return "failed";
-            }
-        }
-    }
-
-    public boolean checkLogout(String token){
-        return tokenRepository.existsByToken(token);
-    }
-
+    String logout(String header);
+    boolean checkLogout(String token);
 }
