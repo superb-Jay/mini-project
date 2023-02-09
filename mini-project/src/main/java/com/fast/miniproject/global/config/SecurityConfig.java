cd2638a21a2afc161fc77a0b01d9a3cf6d4d5cd2
cutegyuseok <yigyuseok@gmail.com>
2023-02-10 02:15:09 +0900

    feat : SecurityConfig Customize

---
+++ mini-project/src/main/java/com/fast/miniproject/global/config/SecurityConfig.java
@@ -0,0 +1,51 @@
+package com.fast.miniproject.global.config;
+
+import com.fast.miniproject.auth.jwt.JwtFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+public class SecurityConfig {
+
+    private final JwtFilter jwtFilter;
+    private static final String[] PUBLIC_URLS = {
+            "/register", "/login" , "/index", "/products**"
+    };
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http)throws Exception{
+
+        return http
+                .authorizeRequests()
+                .mvcMatchers(PUBLIC_URLS).permitAll()
+                .and()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .csrf().disable()
+                .httpBasic().disable()
+                .formLogin().loginPage("/login").permitAll()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilterBefore(
+                        jwtFilter,
+                        UsernamePasswordAuthenticationFilter.class).build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() { //시큐리티 filter 제외, 그러나 OncePerRequestFilter는 시큐리티 필터가 아니라서 로직실행
+        return (web) -> web.ignoring().mvcMatchers(PUBLIC_URLS);
+    }
+
+
+}
