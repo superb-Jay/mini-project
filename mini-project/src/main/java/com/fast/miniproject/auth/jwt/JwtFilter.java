3c2647e335241ad0c6355e0eaaf98dce22e18a64
cutegyuseok <yigyuseok@gmail.com>
2023-02-10 02:08:23 +0900

    feat :Create jwt Config

---
+++ mini-project/src/main/java/com/fast/miniproject/auth/jwt/JwtFilter.java
@@ -0,0 +1,24 @@
+package com.fast.miniproject.auth.jwt;
+
+import io.jsonwebtoken.Claims;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    }
+}
---
+++ mini-project/src/main/java/com/fast/miniproject/auth/jwt/JwtProvider.java
@@ -0,0 +1,45 @@
+package com.fast.miniproject.auth.jwt;
+
+import com.fast.miniproject.auth.entity.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtProvider {
+
+    public String token(User user){
+        Date date=new Date();
+
+        return Jwts.builder()
+                .setHeaderParam("typ","JWT")
+                .setIssuer("Mini")
+                .setIssuedAt(date)
+                .setExpiration(new Date(date.getTime()+3600000))
+                .claim("email",user.getEmail())
+                .claim("name",user.getName())
+                .claim("age",user.getAge())
+                .claim("salary",user.getSalary())
+                .signWith(SignatureAlgorithm.HS256,"23029")
+                .compact();
+    }
+
+    public Claims tokenToUser(String token){
+
+        if(token!=null) {
+            token = token.substring("Bearer".length());
+
+            return Jwts.parser()
+                    .setSigningKey("23029")
+                    .parseClaimsJws(token)
+                    .getBody();
+        }else{
+            return null;
+        }
+
+    }
+
+}
