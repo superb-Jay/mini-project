0e806a918b2f8b4fa8a4e1e988468ba1ce76eed0
Jay <myride.jay@gmail.com>
2023-02-13 17:34:00 +0900

    fix: indate TimeStamp 어노테이션 추가

--- mini-project/src/main/java/com/fast/miniproject/auth/entity/Token.java
+++ mini-project/src/main/java/com/fast/miniproject/auth/entity/Token.java
@@ -1,12 +1,15 @@
 package com.fast.miniproject.auth.entity;
 
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name="logout")
@@ -22,6 +25,7 @@ public class Token {
     @Column(name="token")
     private String token;
 
+    @CreationTimestamp
     @Column(name = "indate")
-    private String inDate;
+    private LocalDateTime indate;
 }
