01a5d0335b2fb6485c02b29a556b8c4112725b07
Jay <myride.jay@gmail.com>
2023-02-13 17:35:42 +0900

    feat: save 전 email 확인 추가

--- mini-project/src/main/java/com/fast/miniproject/auth/repository/UserRepository.java
+++ mini-project/src/main/java/com/fast/miniproject/auth/repository/UserRepository.java
@@ -8,6 +8,9 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User,Long> {
 
     Optional<User> findByEmailAndPassword(String email,String password);
+    Optional<User> findByEmail(String email);
+
+
 
     boolean existsByEmail(String email);
 
