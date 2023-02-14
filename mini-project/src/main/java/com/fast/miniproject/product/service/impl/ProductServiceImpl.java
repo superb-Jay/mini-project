9122d4c4123dbd566537dcf4e61384d71aa3846b
BigFoot <zan04259@naver.com>
2023-02-14 14:38:06 +0900

    feat: ProductService 추가

--- mini-project/src/main/java/com/fast/miniproject/product/service/impl/ProductServiceImpl.java
+++ mini-project/src/main/java/com/fast/miniproject/product/service/impl/ProductServiceImpl.java
@@ -2,12 +2,16 @@ package com.fast.miniproject.product.service.impl;
 
 import com.fast.miniproject.global.response.ErrorResponseDTO;
 import com.fast.miniproject.global.response.ResponseDTO;
+import com.fast.miniproject.product.dto.ProductDTO;
 import com.fast.miniproject.product.dto.ProductDetailDTO;
 import com.fast.miniproject.product.entity.Product;
 import com.fast.miniproject.product.repository.ProductRepository;
 import com.fast.miniproject.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -25,5 +29,20 @@ public class ProductServiceImpl implements ProductService {
         }catch (Exception e){
             return new ErrorResponseDTO(500,"해당 상품의 상세정보를 찾지 못했습니다.").toResponse();
         }
+    }
+
+
+    @Override
+    public ResponseDTO<?> selectProduct() {
+
+        List<Product> product = productRepository.findAll();
+
+
+        List<ProductDTO> productList = product.stream()
+                .map(pro -> new ProductDTO(pro.getPrice(),pro.getBrand(),pro.getLogo(),pro.getName(),pro.getRate(),pro.getDetail()))
+                .collect(Collectors.toList());
+
+        return new ResponseDTO<>(productList);
+
     }
 }
