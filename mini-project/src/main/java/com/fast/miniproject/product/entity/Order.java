7901bf5009f9a927139d84085a2201354723197a
cutegyuseok <yigyuseok@gmail.com>
2023-02-14 19:59:52 +0900

    feat: 상품 구매 Entity

---
+++ mini-project/src/main/java/com/fast/miniproject/product/entity/Order.java
@@ -0,0 +1,36 @@
+package com.fast.miniproject.product.entity;
+
+import com.fast.miniproject.auth.entity.User;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Setter
+@Entity
+@Table(name="orders")
+@ToString
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id")
+    private Long orderId;
+
+    @ManyToOne(targetEntity= User.class, fetch=FetchType.LAZY)
+    @JoinColumn(name="member")
+    private User user;
+
+    @CreationTimestamp
+    @Column(name = "order_date")
+    private LocalDateTime order_date;
+
+    @Builder
+    public Order(User user) {
+        this.user = user;
+    }
+}
---
+++ mini-project/src/main/java/com/fast/miniproject/product/entity/PurchasedProduct.java
@@ -0,0 +1,54 @@
+package com.fast.miniproject.product.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Setter
+@Entity
+@Table(name = "purchased_product")
+public class PurchasedProduct {
+
+    @Id
+    @Column(name = "purchased_product_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long purchasedProductId;
+
+    @Column(name = "purchased_product_price")
+    private int purchasedProductPrice;
+
+    @Column(name = "purchased_product_brand")
+    private String purchasedProductBrand;
+
+    @Column(name = "purchased_product_logo")
+    private String purchasedProductLogo;
+
+    @Column(name = "purchased_product_name")
+    private String purchasedProductName;
+
+    @Column(name = "purchased_product_rate")
+    private double purchasedProductRate;
+
+    @Column(name="purchased_product_detail")
+    private String purchasedProductDetail;
+
+    @ManyToOne(targetEntity=Product.class, fetch=FetchType.LAZY)
+    @JoinColumn(name="product")
+    private Product product;
+
+
+    public PurchasedProduct(Product product) {
+        this.purchasedProductPrice = product.getPrice();
+        this.purchasedProductBrand = product.getBrand();
+        this.purchasedProductLogo = product.getLogo();
+        this.purchasedProductName = product.getName();
+        this.purchasedProductRate = product.getRate();
+        this.purchasedProductDetail = product.getDetail();
+        this.product = product;
+    }
+}
