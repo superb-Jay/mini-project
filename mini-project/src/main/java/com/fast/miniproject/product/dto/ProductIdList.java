d29b7d5caa12a7ab26dc06f2f54c30bf5efb630d
cutegyuseok <yigyuseok@gmail.com>
2023-02-14 20:00:23 +0900

    feat: 상품 구매 Dto

---
+++ mini-project/src/main/java/com/fast/miniproject/product/dto/ProductIdList.java
@@ -0,0 +1,17 @@
+package com.fast.miniproject.product.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductIdList {
+
+    ArrayList<Integer> products_id_list;
+}
