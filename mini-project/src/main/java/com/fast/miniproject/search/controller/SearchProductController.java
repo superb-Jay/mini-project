15ed6c6a6b001af36ca07f146fdf372528705a55
zoomin3022 <zoomin3022@gmail.com>
2023-02-16 13:58:05 +0900

    feat: #25 페이징 처리에 필요한 SearchProductController 변경
    페이지 정상처리, 페이지 번호를 이상하게 줬을때의 예외처리, size를 전역으로 설정 하였는데 나중에 api마다 요구 size가 달라지면 수정 가능(사용자가 size를 임의로 설정하는것도 방지)

--- mini-project/src/main/java/com/fast/miniproject/search/controller/SearchProductController.java
+++ mini-project/src/main/java/com/fast/miniproject/search/controller/SearchProductController.java
@@ -1,26 +1,39 @@
 package com.fast.miniproject.search.controller;
 
+import com.fast.miniproject.global.response.PageResponseDTO;
 import com.fast.miniproject.global.response.ResponseDTO;
-import com.fast.miniproject.product.dto.ProductDTO;
 import com.fast.miniproject.search.service.SearchProductService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/search")
+@Slf4j
 public class SearchProductController {
 
     private final SearchProductService searchProductService;
+    public static final int PAGE_SIZE = 5;
 
     @GetMapping
-    public ResponseDTO<List<ProductDTO>> getProductByName(@RequestParam String name) {
-        return new ResponseDTO<List<ProductDTO>>(searchProductService.searchProductsByName(name));
-    }
+    public ResponseDTO<PageResponseDTO> getProductByName(@RequestParam String name, @RequestParam(required = false, defaultValue = "0") String page) {
+        PageRequest pageRequest = null;
+        try {
+            int intPage = Integer.parseInt(page);
+            pageRequest = PageRequest.of(intPage, PAGE_SIZE);
+            //정상적인 범위 내의 페이지 번호면 해당 페이지로
+        } catch (IllegalArgumentException e) {
+            pageRequest = PageRequest.of(0, PAGE_SIZE);
+            //음수나 오버플로 발생시키는 페이지 번호면 0번페이지로
+            log.info(e.getMessage());
+        }
 
+        PageResponseDTO pageResponseDTO = new PageResponseDTO(searchProductService.searchProductsByName(name, pageRequest));
+        return new ResponseDTO<PageResponseDTO>(pageResponseDTO);
+    }
 }
