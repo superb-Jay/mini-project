0a0a534442a7d07803188ab451f3f7ec4a05f6bc
lala9663 <lala96632040@gmail.com>
2023-02-17 21:10:18 +0900

    feat: 장바구니,위시리스트 swagger 기능

--- mini-project/src/main/java/com/fast/miniproject/product/controller/WishlistController.java
+++ mini-project/src/main/java/com/fast/miniproject/product/controller/WishlistController.java
@@ -6,6 +6,10 @@ import com.fast.miniproject.product.dto.WishlistAddRequestDTO;
 import com.fast.miniproject.product.dto.WishlistDeleteRequestDTO;
 import com.fast.miniproject.product.service.BasketService;
 import com.fast.miniproject.product.service.WishlistService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,6 +17,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(tags = {"위시리스트 기능"}, description = "상품 추가, 상품 삭제")
 @RestController
 @RequiredArgsConstructor
 public class WishlistController {
@@ -20,21 +25,34 @@ public class WishlistController {
     private final BasketService basketService;
 
     @GetMapping("/wishlist")
+    @ApiOperation(value = "위시리스트 (토큰 0)", notes = "상품 목록 조회")
     public ResponseDTO<?> listWishlist(@AuthenticationPrincipal LoginReqDTO loginReqDTO) {
         return wishlistService.listWishlistDTO(loginReqDTO);
     }
 
     @PostMapping("/wishlist/add")
+    @ApiOperation(value = "위시리스트에 상품 추가 (토큰 0)", notes = "productId를 통한 상품 추가")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "productId", value = "productId", required = true)
+    })
     public ResponseDTO<?> addWishlist(@AuthenticationPrincipal LoginReqDTO loginReqDTO, WishlistAddRequestDTO requestDTO) {
         return wishlistService.addWishlist(loginReqDTO, requestDTO);
     }
 
     @DeleteMapping("/wishlist/delete")
+    @ApiOperation(value = "위시리스트에 상품 삭제 (토큰 0)", notes = "basketId를 통한 상품 삭제")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "basketId", value = "basketId", required = true),
+    })
     public ResponseDTO<?> deleteWishlist(@AuthenticationPrincipal LoginReqDTO loginReqDTO, WishlistDeleteRequestDTO requestDTO) {
         return wishlistService.deleteWishlist(loginReqDTO, requestDTO);
     }
 
     @PostMapping("/wishlist/addBasket")
+    @ApiOperation(value = "위시리스트에서 장바구니에 추가 (토큰 0)", notes = "productId를 통한 상품 추가")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "productId", value = "productId", required = true)
+    })
     public ResponseDTO<?> addBasketFromWishlist(@AuthenticationPrincipal LoginReqDTO loginReqDTO, WishlistAddRequestDTO requestDTO) {
         return wishlistService.addBasketFromWishlist(loginReqDTO, requestDTO);
     }
