f64f7b58bad1247953348d8e31e9dde9f0bb068c
cutegyuseok <yigyuseok@gmail.com>
2023-02-17 19:29:44 +0900

    feat: 구매 후 장바구니에서 구매한 상품만 삭제

--- mini-project/src/main/java/com/fast/miniproject/product/repository/BasketRepository.java
+++ mini-project/src/main/java/com/fast/miniproject/product/repository/BasketRepository.java
@@ -4,6 +4,9 @@ import com.fast.miniproject.auth.entity.User;
 import com.fast.miniproject.product.entity.Basket;
 import com.fast.miniproject.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -16,5 +19,10 @@ public interface BasketRepository extends JpaRepository<Basket, Long> {
     Boolean existsByProductAndUser(Product product, User user);
 
     List<Basket> findAllByUser(User user);
+
+    @Modifying
+    @Query(nativeQuery = true ,value = "DELETE FROM basket WHERE member =:member AND product IN (:id_list)")
+    void deleteByUserAndProductList(@Param("member") Long member,@Param("id_list") List<Integer> id_list);
+
 
 }
