2d776a2b62439ced329844b0e9c04ae7d4271316
Jay <myride.jay@gmail.com>
2023-02-18 04:46:14 +0900

    Revert "feat: swagger JWT 토큰 입력 기능 추가"
    This reverts commit 90880615c53e8ebb3a6e84cf5cf61dd8d927edc9.

--- mini-project/src/main/java/com/fast/miniproject/global/config/SwaggerConfig.java
+++ mini-project/src/main/java/com/fast/miniproject/global/config/SwaggerConfig.java
@@ -8,60 +8,33 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.*;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 @EnableWebMvc
 @Configuration
 public class SwaggerConfig extends WebMvcConfigurationSupport {
 
     @Bean
-    public Docket api() {
+    public Docket api(TypeResolver typeResolver) {
         return new Docket(DocumentationType.OAS_30) // 3.0 문서버전으로 세팅
-                .groupName("미니프로젝트 3조")
+                .apiInfo(apiInfo())
+                .groupName("Test")
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.fast.miniproject"))
-//                .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
-                .useDefaultResponseMessages(false)
-                .apiInfo(apiInfo())
-                .securityContexts(Collections.singletonList(securityContext()))
-                .securitySchemes(List.of(apiKey()));
-
+                .useDefaultResponseMessages(false);
     }
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("3조 미니 프로젝트 백앤드")
                 .description("금융 상품 추천 서비스 페이지 제작 ")
-//                .contact(new Contact("이름","홈페이지","email"))
 //                .license("라이센스소유자")
 //                .licenseUrl("라이센스URL")
                 .version("1.0")
                 .build();
-    }
-
-    //ApiKey 정의
-    private ApiKey apiKey() {
-        return new ApiKey("JWT", "Authorization", "header");
-    }
-
-    //JWT SecurityContext 구성
-    private SecurityContext securityContext() {
-        return SecurityContext.builder().securityReferences(defaultAuth()).build();
-    }
-
-    private List<SecurityReference> defaultAuth() {
-        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEveryThing");
-        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-        authorizationScopes[0] = authorizationScope;
-        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
     }
 }
