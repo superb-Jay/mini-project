5106228b20e475fb6b758ad4a33c6403ac5b92d5
cutegyuseok <yigyuseok@gmail.com>
2023-02-18 17:36:29 +0900

    feat : ProductController API Renamed

--- mini-project/src/main/java/com/fast/miniproject/product/controller/ProductController.java
+++ mini-project/src/main/java/com/fast/miniproject/product/controller/ProductController.java
@@ -22,38 +22,37 @@ public class ProductController {
     private final ProductService productService;
 
     @ApiOperation(value = "상품 상세 정보 반환(토큰X)", notes = "상품 상세 정보를 반환하는 API")
-
-    @GetMapping("/product/detail")
+    @GetMapping("/api/products/details")
     public ResponseDTO<?> productDetail(@RequestParam Long products_id){
         return productService.selectProductDetail(products_id);
     }
 
     @ApiOperation(value = "상품 전체 리스트 반환", notes = "상품 전체 리스트 반환하는 API")
-    @GetMapping("/products")
+    @GetMapping("/api/products")
     public ResponseDTO<?> selectProduct( ){
         return productService.selectProduct();
     }
 
     @ApiOperation(value = "상품 추천 리스트 반환", notes = "사용자가 가입 가능한 상품 리스트를 반환하는 API")
-    @GetMapping("/product/recommend")
+    @GetMapping("/api/products/recommend")
     public ResponseDTO<?> recommendProduct(@ApiIgnore @AuthenticationPrincipal LoginReqDTO loginReqDTO){
         return productService.recommendProduct(loginReqDTO.getEmail());
     }
 
-    @PostMapping("/products/buy")
+    @PostMapping("/api/orders")
     @ApiOperation(value = "상품(들) 구매", notes = "상품(들)을 구매하는 API")
 
     public ResponseDTO<?> buyProducts(@ApiIgnore @AuthenticationPrincipal LoginReqDTO user, @RequestBody ProductIdList products_id_list){
         return productService.buyProduct(products_id_list.getProducts_id_list(),user);
     }
 
-    @GetMapping("/product/order/check")
+    @GetMapping("/api/orders")
     @ApiOperation(value = "구매 기록 반환", notes = "상품을 구매한 기록을 반환하는 API")
     public ResponseDTO<?> orderCheck(@ApiIgnore @AuthenticationPrincipal LoginReqDTO dto){
          return productService.orderCheck(dto);
     }
 
-    @DeleteMapping("/product/order/delete")
+    @DeleteMapping("/api/orders")
     @ApiOperation(value = "주문 취소", notes = "주문을 취소하는 API")
 
     public ResponseDTO<?> deleteBuy(@ApiIgnore @AuthenticationPrincipal LoginReqDTO dto,@RequestParam Long orderId){
