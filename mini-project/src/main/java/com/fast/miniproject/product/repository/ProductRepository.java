7c61ed77b8d05b1b6eb8c844cfbe2e8c4c12fea5
zoomin3022 <zoomin3022@gmail.com>
2023-02-20 13:43:04 +0900

    feat: #25 /api/products 페이징 처리를 위한 ProductRepository 변경
    검색 리턴 타입 List -> Page

--- mini-project/src/main/java/com/fast/miniproject/product/repository/ProductRepository.java
+++ mini-project/src/main/java/com/fast/miniproject/product/repository/ProductRepository.java
@@ -1,6 +1,8 @@
 package com.fast.miniproject.product.repository;
 
 import com.fast.miniproject.product.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -16,4 +18,6 @@ public interface ProductRepository extends JpaRepository<Product,Long> {
     List<Product> findAllByProductId(@Param("id_list") List<Integer> id_list);
 
     Optional<Product> findByProductId(Long id);
+
+    Page<Product> findAll(Pageable pageable);
 }
