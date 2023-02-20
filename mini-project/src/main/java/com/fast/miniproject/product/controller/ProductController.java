862824b0606fcd00c1b4c413a6d05ed72b5bf013
zoomin3022 <zoomin3022@gmail.com>
2023-02-20 14:00:40 +0900

    feat: #25 /api/products 페이징 처리를 위한 ProductController 변경

--- mini-project/src/main/java/com/fast/miniproject/product/controller/ProductController.java
+++ mini-project/src/main/java/com/fast/miniproject/product/controller/ProductController.java
@@ -1,17 +1,18 @@
 package com.fast.miniproject.product.controller;
 
 import com.fast.miniproject.auth.dto.LoginReqDTO;
+import com.fast.miniproject.global.response.ResponseDTO;
 import com.fast.miniproject.product.dto.ProductIdList;
+import com.fast.miniproject.product.service.ProductService;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-import com.fast.miniproject.global.response.ResponseDTO;
-import com.fast.miniproject.product.service.ProductService;
-import lombok.RequiredArgsConstructor;
 import springfox.documentation.annotations.ApiIgnore;
+
+import static com.fast.miniproject.search.controller.SearchProductController.PAGE_SIZE;
 
 @Api(tags = {"상품 관련 서비스"}, description = "상품 전체 조회, 상품 상세 조회, 상품 추천, 주문, 주문 확인, 주문 취소")
 @RestController
@@ -23,39 +24,46 @@ public class ProductController {
 
     @ApiOperation(value = "상품 상세 정보 반환(토큰X)", notes = "상품 상세 정보를 반환하는 API")
     @GetMapping("/api/products/details")
-    public ResponseDTO<?> productDetail(@RequestParam Long products_id){
+    public ResponseDTO<?> productDetail(@RequestParam Long products_id) {
         return productService.selectProductDetail(products_id);
     }
 
     @ApiOperation(value = "상품 전체 리스트 반환", notes = "상품 전체 리스트 반환하는 API")
     @GetMapping("/api/products")
-    public ResponseDTO<?> selectProduct( ){
-        return productService.selectProduct();
+    public ResponseDTO<?> selectProduct(@RequestParam(required = false, defaultValue = "1") String page) {
+        PageRequest pageRequest = null;
+        try {
+            int intPage = Integer.parseInt(page);
+            pageRequest = PageRequest.of(intPage - 1, PAGE_SIZE);
+        } catch (IllegalArgumentException e) {
+            pageRequest = PageRequest.of(0, PAGE_SIZE);
+        }
+        return productService.selectProduct(pageRequest);
     }
 
     @ApiOperation(value = "상품 추천 리스트 반환", notes = "사용자가 가입 가능한 상품 리스트를 반환하는 API")
     @GetMapping("/api/products/recommends")
-    public ResponseDTO<?> recommendProduct(@ApiIgnore @AuthenticationPrincipal LoginReqDTO loginReqDTO){
+    public ResponseDTO<?> recommendProduct(@ApiIgnore @AuthenticationPrincipal LoginReqDTO loginReqDTO) {
         return productService.recommendProduct(loginReqDTO.getEmail());
     }
 
     @PostMapping("/api/orders")
     @ApiOperation(value = "상품(들) 구매", notes = "상품(들)을 구매하는 API")
 
-    public ResponseDTO<?> buyProducts(@ApiIgnore @AuthenticationPrincipal LoginReqDTO user, @RequestBody ProductIdList products_id_list){
-        return productService.buyProduct(products_id_list.getProducts_id_list(),user);
+    public ResponseDTO<?> buyProducts(@ApiIgnore @AuthenticationPrincipal LoginReqDTO user, @RequestBody ProductIdList products_id_list) {
+        return productService.buyProduct(products_id_list.getProducts_id_list(), user);
     }
 
     @GetMapping("/api/orders")
     @ApiOperation(value = "구매 기록 반환", notes = "상품을 구매한 기록을 반환하는 API")
-    public ResponseDTO<?> orderCheck(@ApiIgnore @AuthenticationPrincipal LoginReqDTO dto){
-         return productService.orderCheck(dto);
+    public ResponseDTO<?> orderCheck(@ApiIgnore @AuthenticationPrincipal LoginReqDTO dto) {
+        return productService.orderCheck(dto);
     }
 
     @DeleteMapping("/api/orders")
     @ApiOperation(value = "주문 취소", notes = "주문을 취소하는 API")
 
-    public ResponseDTO<?> deleteBuy(@ApiIgnore @AuthenticationPrincipal LoginReqDTO dto,@RequestParam Long orderId){
-        return productService.deleteOrder(dto,orderId);
+    public ResponseDTO<?> deleteBuy(@ApiIgnore @AuthenticationPrincipal LoginReqDTO dto, @RequestParam Long orderId) {
+        return productService.deleteOrder(dto, orderId);
     }
 }
