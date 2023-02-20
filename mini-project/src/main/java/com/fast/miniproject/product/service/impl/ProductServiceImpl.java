6807f1e89ecccc15a55b8a2aaa2998928af5d056
zoomin3022 <zoomin3022@gmail.com>
2023-02-20 13:47:21 +0900

    feat: #25 /api/products 페이징 처리를 위한 ProductService, Impl 변경
    ResponseDTO의 Data를 Page<ProductDTO>로 변경

--- mini-project/src/main/java/com/fast/miniproject/product/service/impl/ProductServiceImpl.java
+++ mini-project/src/main/java/com/fast/miniproject/product/service/impl/ProductServiceImpl.java
@@ -4,6 +4,7 @@ import com.fast.miniproject.auth.dto.LoginReqDTO;
 import com.fast.miniproject.auth.entity.User;
 import com.fast.miniproject.auth.repository.UserRepository;
 import com.fast.miniproject.global.response.ErrorResponseDTO;
+import com.fast.miniproject.global.response.PageResponseDTO;
 import com.fast.miniproject.global.response.ResponseDTO;
 import com.fast.miniproject.product.dto.OrderHistory;
 import com.fast.miniproject.product.dto.ProductDTO;
@@ -18,6 +19,8 @@ import com.fast.miniproject.product.repository.ProductRepository;
 import com.fast.miniproject.product.repository.PurchaseProductRepository;
 import com.fast.miniproject.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -43,42 +46,40 @@ public class ProductServiceImpl implements ProductService {
             ProductDetailDTO productDetailDTO = new ProductDetailDTO(product);
             return new ResponseDTO<>(productDetailDTO);
 
-        }catch (Exception e){
-            return new ErrorResponseDTO(500,"해당 상품의 상세정보를 찾지 못했습니다.").toResponse();
+        } catch (Exception e) {
+            return new ErrorResponseDTO(500, "해당 상품의 상세정보를 찾지 못했습니다.").toResponse();
         }
     }
 
     @Override
-    public ResponseDTO<?> selectProduct() {
-        try{
-            List<Product> product = productRepository.findAll();
-            List<ProductDTO> productList = product.stream()
-                    .map(pro -> new ProductDTO(pro.getPrice(),pro.getBrand(),pro.getLogo(),pro.getName(),pro.getRate(),pro.getDetail(),pro.getProductId()))
-                    .collect(Collectors.toList());
-            return new ResponseDTO<>(productList);
-        }catch(Exception e){
-            return new ErrorResponseDTO(500,"상품 목록을 불러오지 못 했습니다").toResponse();
+    public ResponseDTO<?> selectProduct(Pageable pageable) {
+        try {
+            Page<ProductDTO> productDTO = productRepository.findAll(pageable)
+                    .map(ProductDTO::new);
+            return new ResponseDTO<>(new PageResponseDTO(productDTO));
+        } catch (Exception e) {
+            return new ErrorResponseDTO(500, "상품 목록을 불러오지 못 했습니다").toResponse();
         }
     }
 
     @Override
     public ResponseDTO<?> recommendProduct(String email) {
 
-        try{
+        try {
 
-            User user  = userRepository.findByEmail(email).get();
+            User user = userRepository.findByEmail(email).get();
 
-            int limitAmount =(int) (user.getSalary() * 2);
+            int limitAmount = (int) (user.getSalary() * 2);
 
             List<Product> product = productRepository.findByPriceLessThanEqual(limitAmount);
 
             List<ProductDTO> productList = product.stream()
-                    .map(pro -> new ProductDTO(pro.getPrice(),pro.getBrand(),pro.getLogo(),pro.getName(),pro.getRate(),pro.getDetail(),pro.getProductId()))
+                    .map(pro -> new ProductDTO(pro.getPrice(), pro.getBrand(), pro.getLogo(), pro.getName(), pro.getRate(), pro.getDetail(), pro.getProductId()))
                     .collect(Collectors.toList());
 
             return new ResponseDTO<>(productList);
-        }catch(Exception e){
-            return new ErrorResponseDTO(500,"추천 상품을 불러 오지 못 했습니다").toResponse();
+        } catch (Exception e) {
+            return new ErrorResponseDTO(500, "추천 상품을 불러 오지 못 했습니다").toResponse();
         }
     }
 
@@ -87,16 +88,18 @@ public class ProductServiceImpl implements ProductService {
     public ResponseDTO<?> buyProduct(ArrayList<Integer> products_id_list, LoginReqDTO dto) {
         User user = userRepository.findByEmail(dto.getEmail()).orElseThrow(IllegalArgumentException::new);
         List<Product> productList = productRepository.findAllByProductId(products_id_list);
-        if (productList.size()!=products_id_list.size())return new ErrorResponseDTO(500,"구매에 실패하였습니다.").toResponse();
-        if(!isAvailableToPurchase(user,productList))return new ErrorResponseDTO(500,"대출 가능 금액을 초과하였습니다.").toResponse();
+        if (productList.size() != products_id_list.size())
+            return new ErrorResponseDTO(500, "구매에 실패하였습니다.").toResponse();
+        if (!isAvailableToPurchase(user, productList))
+            return new ErrorResponseDTO(500, "대출 가능 금액을 초과하였습니다.").toResponse();
         try {
             Orders orders = new Orders(user);
-            toSave(productList,orders);
+            toSave(productList, orders);
             orderRepository.save(orders);
-            basketRepository.deleteByUserAndProductList(user.getMemberId(),products_id_list);
+            basketRepository.deleteByUserAndProductList(user.getMemberId(), products_id_list);
             return new ResponseDTO<>("상품 구매에 성공하였습니다.");
-        }catch (Exception e){
-            return new ErrorResponseDTO(500,"구매에 실패하였습니다.").toResponse();
+        } catch (Exception e) {
+            return new ErrorResponseDTO(500, "구매에 실패하였습니다.").toResponse();
         }
     }
 
@@ -104,75 +107,74 @@ public class ProductServiceImpl implements ProductService {
     public ResponseDTO<?> orderCheck(LoginReqDTO dto) {
         User user = userRepository.findByEmail(dto.getEmail()).orElseThrow(IllegalArgumentException::new);
         try {
-           List<Orders> orderList =  orderRepository.findAllByUserOrderByPurchaseDate(user);
-           ArrayList<PurchasedProduct> list = purchaseProductRepository.findAllByOrdersList(orderList);
-           return new ResponseDTO<>(parsingByOrders(list));
-        }catch (Exception e){
+            List<Orders> orderList = orderRepository.findAllByUserOrderByPurchaseDate(user);
+            ArrayList<PurchasedProduct> list = purchaseProductRepository.findAllByOrdersList(orderList);
+            return new ResponseDTO<>(parsingByOrders(list));
+        } catch (Exception e) {
 
         }
-        return new ErrorResponseDTO(500,"구매하신 상품이 없습니다.").toResponse();
+        return new ErrorResponseDTO(500, "구매하신 상품이 없습니다.").toResponse();
     }
 
     @Override
     public ResponseDTO<?> deleteOrder(LoginReqDTO dto, Long orderId) {
         User user = userRepository.findByEmail(dto.getEmail()).orElseThrow(IllegalArgumentException::new);
         try {
-            Orders orders = orderRepository.findByOrderIdAndUser(orderId,user).orElseThrow(IllegalArgumentException::new);
+            Orders orders = orderRepository.findByOrderIdAndUser(orderId, user).orElseThrow(IllegalArgumentException::new);
             orderRepository.delete(orders);
-            return new ResponseDTO<>(200,"구매를 취소하였습니다.",null);
-        }catch (Exception e){
+            return new ResponseDTO<>(200, "구매를 취소하였습니다.", null);
+        } catch (Exception e) {
 
         }
-        return new ErrorResponseDTO(500,"구매 취소를 실패하였습니다.").toResponse();
+        return new ErrorResponseDTO(500, "구매 취소를 실패하였습니다.").toResponse();
 
     }
 
-    private boolean isAvailableToPurchase(User user,List<Product> productList){
+    private boolean isAvailableToPurchase(User user, List<Product> productList) {
         List<Orders> ordersList = orderRepository.findAllByUserOrderByPurchaseDate(user);
-       Integer sum =purchaseProductRepository.searchSumByOrdersList(ordersList);
-       int max = (int) (user.getSalary()*2);
-       if (sum!=null){
-           System.out.println(sum);
-           max-=sum;
-       }
-       for (Product product: productList){
-           max-=product.getPrice();
-       }
-       if (max>0) {
-           return true;
-       }else {
-           return false;
-       }
+        Integer sum = purchaseProductRepository.searchSumByOrdersList(ordersList);
+        int max = (int) (user.getSalary() * 2);
+        if (sum != null) {
+            System.out.println(sum);
+            max -= sum;
+        }
+        for (Product product : productList) {
+            max -= product.getPrice();
+        }
+        if (max > 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 
-    private void toSave(List<Product> productList,Orders orders){
-        for (Product product: productList){
+    private void toSave(List<Product> productList, Orders orders) {
+        for (Product product : productList) {
             PurchasedProduct purchasedProduct = new PurchasedProduct(product);
             orders.getPurchasedProducts().add(purchasedProduct);
             purchasedProduct.setOrders(orders);
         }
     }
 
-    private ArrayList<OrderHistory> parsingByOrders(ArrayList<PurchasedProduct> purchasedProducts){
+    private ArrayList<OrderHistory> parsingByOrders(ArrayList<PurchasedProduct> purchasedProducts) {
         ArrayList<OrderHistory> orderHistoryArrayList = new ArrayList<>();
         ArrayList<PurchasedProductDto> toShowList = new ArrayList<>();
         Orders order = null;
-        for (int i=0;i<purchasedProducts.size();i++){
-            if(order==purchasedProducts.get(i).getOrders()||order==null){
-                order=purchasedProducts.get(i).getOrders();
+        for (int i = 0; i < purchasedProducts.size(); i++) {
+            if (order == purchasedProducts.get(i).getOrders() || order == null) {
+                order = purchasedProducts.get(i).getOrders();
                 toShowList.add(new PurchasedProductDto(purchasedProducts.get(i)));
-            }else {
-                orderHistoryArrayList.add(new OrderHistory(toShowList,order));
-                order=purchasedProducts.get(i).getOrders();
+            } else {
+                orderHistoryArrayList.add(new OrderHistory(toShowList, order));
+                order = purchasedProducts.get(i).getOrders();
                 toShowList = new ArrayList<>();
                 toShowList.add(new PurchasedProductDto(purchasedProducts.get(i)));
             }
         }
-        orderHistoryArrayList.add(new OrderHistory(toShowList,order));
+        orderHistoryArrayList.add(new OrderHistory(toShowList, order));
         return orderHistoryArrayList;
     }
-
 
 
 }
