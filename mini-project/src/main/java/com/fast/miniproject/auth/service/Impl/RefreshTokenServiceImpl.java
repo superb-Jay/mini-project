82ac991a96b12efbb74c0c79999d1c056e1fdc40
Jay <myride.jay@gmail.com>
2023-02-22 04:17:08 +0900

    feat: 유효토큰 예외처리 추가

--- mini-project/src/main/java/com/fast/miniproject/auth/service/Impl/RefreshTokenServiceImpl.java
+++ mini-project/src/main/java/com/fast/miniproject/auth/service/Impl/RefreshTokenServiceImpl.java
@@ -1,16 +1,11 @@
 package com.fast.miniproject.auth.service.Impl;
 
 import com.fast.miniproject.auth.dto.TokenDTO;
-import com.fast.miniproject.auth.entity.RefreshToken;
 import com.fast.miniproject.auth.jwt.JwtProvider;
-import com.fast.miniproject.auth.repository.RefreshTokenRepository;
 import com.fast.miniproject.auth.service.RefreshTokenService;
 import com.fast.miniproject.global.response.ResponseDTO;
-import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.Date;
 
 
 @Service
@@ -19,26 +14,23 @@ public class RefreshTokenServiceImpl implements RefreshTokenService {
 
 
     private final JwtProvider jwtProvider;
-    private final RefreshTokenRepository refreshTokenRepository;
-
-
+    private final RedisService redisService;
 
     @Override
     public ResponseDTO<?> validateRefreshToken(String refreshToken) {
         try {
-            RefreshToken updateRefreshToken = refreshTokenRepository.findByRefreshToken(refreshToken)
-                    .orElseThrow(IllegalArgumentException::new);
-            Claims claims = jwtProvider.tokenToClaims(updateRefreshToken.getRefreshToken());
-            if (!claims.getExpiration().before(new Date())) {
-                String updateAccessToken = jwtProvider.recreationAccessToken(claims.get("email").toString());
+            String userEmail = redisService.getData(refreshToken);
+
+            if (jwtProvider.getExpiration(refreshToken) > 0) {
+                String updateAccessToken = jwtProvider.recreationAccessToken(userEmail);
                 return new ResponseDTO<>(200, "Refresh 토큰을 통한 Access Token 생성이 완료되었습니다",
                         TokenDTO.builder().accessToken(updateAccessToken).refreshToken(refreshToken).build());
             }else{
                 throw new IllegalArgumentException();
             }
 
-        }catch (IllegalArgumentException e) {
-            return new ResponseDTO<>(402,"Refresh 토큰이 만료되었습니다. 로그인이 필요합니다.",null);
+        }catch (IllegalArgumentException | NullPointerException e) {
+            return new ResponseDTO<>(403,"Refresh 토큰이 유효하지 않습니다. 로그인이 필요합니다.",null);
         }
     }
 
