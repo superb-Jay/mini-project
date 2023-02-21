f4b31aa21809e1917705dcd428ff60ab5918e7d1
Jay <myride.jay@gmail.com>
2023-02-21 17:36:50 +0900

    style: 필요없는 import 정리

--- mini-project/src/main/java/com/fast/miniproject/auth/jwt/JwtFilter.java
+++ mini-project/src/main/java/com/fast/miniproject/auth/jwt/JwtFilter.java
@@ -2,9 +2,6 @@ package com.fast.miniproject.auth.jwt;
 
 import com.fast.miniproject.auth.dto.LoginReqDTO;
 import com.fast.miniproject.auth.service.TokenService;
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.JwtException;
-import io.jsonwebtoken.SignatureException;
 import lombok.Builder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
@@ -30,20 +27,20 @@ public class JwtFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-            String token = request.getHeader(HttpHeaders.AUTHORIZATION);
+        String token = request.getHeader(HttpHeaders.AUTHORIZATION);
 
-            if (!tokenService.checkLogout(token)) {
+        if (!tokenService.checkLogout(token)) {
 
-                LoginReqDTO loginReqDTO = jwtProvider.tokenToUser(token);
+            LoginReqDTO loginReqDTO = jwtProvider.tokenToUser(token);
 
 //           분석이 끝난 유저 객체에 있는 정보를 시큐리티컨텍스트 빈객체에 넘겨준다. (정보와, 권한을 넘겨준다.)
-                if (loginReqDTO != null) {
-                    SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(
-                            loginReqDTO,
-                            "",
-                            loginReqDTO.getAuthorities()));
-                }
+            if (loginReqDTO != null) {
+                SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(
+                        loginReqDTO,
+                        "",
+                        loginReqDTO.getAuthorities()));
             }
+        }
 
         filterChain.doFilter(request, response);
     }
