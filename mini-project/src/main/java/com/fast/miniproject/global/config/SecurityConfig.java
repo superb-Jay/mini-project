1fc2059dc0558da6adf6293f68a95f3bd1995ec1
Jay <myride.jay@gmail.com>
2023-02-21 18:29:13 +0900

    feat: 토큰검증 필터 추가 설정

--- mini-project/src/main/java/com/fast/miniproject/global/config/SecurityConfig.java
+++ mini-project/src/main/java/com/fast/miniproject/global/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.fast.miniproject.global.config;
 
+import com.fast.miniproject.auth.jwt.JwtExceptionFilter;
 import com.fast.miniproject.auth.jwt.JwtFilter;
+import com.fast.miniproject.auth.jwt.JwtProperties;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -23,6 +25,8 @@ public class SecurityConfig {
 
 
     private final JwtFilter jwtFilter;
+    private final JwtProperties jwtProperties;
+
 
 
     private static final String[] PUBLIC_URLS = { //이 URL은 권한 검사안함
@@ -64,7 +68,9 @@ public class SecurityConfig {
                         // 퍼블릭 URL은 시큐리티 필터에만 해당하므로 모든 요청에 jwt 필터가 먼저 동작하게된다. addFilterbefore 로 인하여.
                         // 물론 퍼블릭 url은 jwt 필터만 통과하면 시큐리티필터는 예외 처리 된다.
                         jwtFilter, // 요청을 할때마다 한번 거쳐가는 필터.
-                        UsernamePasswordAuthenticationFilter.class).build();
+                        UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(JwtExceptionFilter.of(jwtProperties),UsernamePasswordAuthenticationFilter.class)
+                .build();
 
         //인증을 처리하는 기본필터 UsernamePasswordAuthenticationFilter 대신 별도의 인증 로직을 가진 필터를 생성하고 사용하고 싶을 때 아래와 같이 필터를 등록하고 사용
     }
