5f844a39f195a7c0019c62e85b60d6b7c8e5f3f8
Jay <myride.jay@gmail.com>
2023-02-22 15:41:08 +0900

    style: redis 적용으로 RefreshToken 관련 엔티티 삭제

--- mini-project/src/main/java/com/fast/miniproject/auth/dto/TokenDTO.java
+++ mini-project/src/main/java/com/fast/miniproject/auth/dto/TokenDTO.java
@@ -1,6 +1,5 @@
 package com.fast.miniproject.auth.dto;
 
-import com.fast.miniproject.auth.entity.RefreshToken;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,13 +14,6 @@ public class TokenDTO {
 
     private final String accessToken;
     private final String refreshToken;
-
-    public RefreshToken toEntity() {
-        return RefreshToken.builder()
-                .refreshToken(this.refreshToken)
-                .build();
-    }
-
 
 
 }
