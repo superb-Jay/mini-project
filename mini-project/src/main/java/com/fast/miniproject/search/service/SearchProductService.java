a045da0813be88693a1a28f2e3abeadeb6d73fbb
zoomin3022 <zoomin3022@gmail.com>
2023-02-23 20:37:44 +0900

    feat: #72 SearchProductService 이슈 관련 로직 구현

--- mini-project/src/main/java/com/fast/miniproject/search/service/SearchProductService.java
+++ mini-project/src/main/java/com/fast/miniproject/search/service/SearchProductService.java
@@ -1,7 +1,14 @@
 package com.fast.miniproject.search.service;
 
+import com.fast.miniproject.auth.dto.LoginReqDTO;
+import com.fast.miniproject.auth.entity.User;
+import com.fast.miniproject.auth.exception.UserException;
+import com.fast.miniproject.auth.exception.UserExceptionType;
+import com.fast.miniproject.auth.repository.UserRepository;
 import com.fast.miniproject.product.dto.ProductDTO;
+import com.fast.miniproject.product.service.ProductService;
 import com.fast.miniproject.search.repository.SearchProductRepository;
+import com.querydsl.core.BooleanBuilder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -12,9 +19,18 @@ import org.springframework.stereotype.Service;
 public class SearchProductService {
 
     private final SearchProductRepository searchProductRepository;
+    private final UserRepository userRepository;
+    private final ProductService productService;
 
     public Page<ProductDTO> searchProducts(String searchTarget, String searchKeyword, String sortTarget, String sortKeyword, Pageable pageable) {
-        return searchProductRepository.searchQuery(searchTarget, searchKeyword, sortTarget, sortKeyword, pageable)
+        return searchProductRepository.searchQuery(searchTarget, searchKeyword, sortTarget, sortKeyword, pageable, null)
+                .map(ProductDTO::new);
+    }
+
+    public Page<ProductDTO> searchProducts(String searchTarget, String searchKeyword, String sortTarget, String sortKeyword, Pageable pageable, LoginReqDTO loginReqDTO) {
+        User user = userRepository.findByEmail(loginReqDTO.getEmail()).orElseThrow(() -> new UserException(UserExceptionType.NON_EXISTENT_USER));
+        BooleanBuilder booleanBuilder = new BooleanBuilder(searchProductRepository.searchQueryWithUser(productService.availableAmount(user)));
+        return searchProductRepository.searchQuery(searchTarget, searchKeyword, sortTarget, sortKeyword, pageable, booleanBuilder)
                 .map(ProductDTO::new);
     }
 
